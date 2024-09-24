a188f058d34cb404664bfde813ea470ae6027ef5
Nuray <nuraynurzhaskyzy@gmail.com>
2024-09-24 23:18:00 +0500

    feature: Add Seed for User table

---
+++ prisma/seeds/seedUser.ts
@@ -0,0 +1,44 @@
+import { PrismaClient, Role } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+interface Users {
+  email: string;
+  password: string;
+  role: Role;
+}
+
+export async function UserSeed() {
+  const users: Users[] = [
+    { email: 'admin@elab.com', password: 'Admin_123', role: Role.Admin },
+    { email: 'nurai@elab.com', password: 'Nurai_456', role: Role.Admin },
+    {
+      email: 'arman@elab.com',
+      password: 'Arman_789',
+      role: Role.ProcurementOfficer,
+    },
+    {
+      email: 'bob@elab.com',
+      password: 'Bob_123',
+      role: Role.ProcurementOfficer,
+    },
+    { email: 'jane@elab.com', password: 'Jane_432', role: Role.Researcher },
+    { email: 'lena@elab.com', password: 'Lena_876', role: Role.Researcher },
+    { email: 'mark@elab.com', password: 'Mark_739', role: Role.Researcher },
+    { email: 'jack@elab.com', password: 'Jack_720', role: Role.Researcher },
+    { email: 'karya@elab.com', password: 'Karya_007', role: Role.Researcher },
+  ];
+
+  for (const user of users) {
+    await prisma.user.upsert({
+      where: { email: user.email },
+      update: {},
+      create: {
+        email: user.email,
+        password: await bcrypt.hash(user.password, 10),
+        role: user.role,
+      },
+    });
+  }
+}
