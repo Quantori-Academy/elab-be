0d15b28159193b4b8daff3fdf0b6f615bb249dcf
armanmikoyan <arman.mikoyan1@gmail.com>
2024-09-26 17:08:40 +0400

    type correction

--- src/modules/auth/interfaces/authService.interface.ts
+++ src/modules/auth/interfaces/authService.interface.ts
@@ -1,5 +1,6 @@
+import { Tokens } from 'src/modules/security/interfaces/token.interface';
 import { IUser } from 'src/modules/user/interfaces/userEntity.interface';
 
 export interface IAuthService {
-  login(user: IUser): Promise<any>;
+  login(user: IUser): Promise<Tokens>;
 }
