c01fcccd5adf1058f3a53c5138cf8c3c25f87f68
armanmikoyan <arman.mikoyan1@gmail.com>
2024-09-27 20:46:47 +0400

    elab-be #14: extend security service

--- src/modules/security/interfaces/securityService.interface.ts
+++ src/modules/security/interfaces/securityService.interface.ts
@@ -1,8 +1,10 @@
+import { UserPayload } from 'src/modules/user/interfaces/userEntity.interface';
 import { AccessToken, RefreshToken } from './token.interface';
 
 export interface ISecurityService {
   generateAccessToken(payload: any): Promise<AccessToken>;
   generateRefreshToken(payload: any): Promise<RefreshToken>;
+  verifyRefreshToken(token: RefreshToken): Promise<UserPayload>;
   hash(entity: any, salt: number): Promise<string>;
   compare(raw: any, hashed: any): Promise<boolean>;
 }
