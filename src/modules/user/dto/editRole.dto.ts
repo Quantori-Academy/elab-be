9eb068cf0967c9ea8dc1187579cc4cb80684be7d
armanmikoyan <arman.mikoyan1@gmail.com>
2024-09-28 16:46:30 +0400

    elab-be-20 #20: swagger doc fro api

--- src/modules/user/dto/editRole.dto.ts
+++ src/modules/user/dto/editRole.dto.ts
@@ -1,7 +1,47 @@
 import { IsEnum } from 'class-validator';
 import { Role } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class EditRoleDto {
+class EditRoleDto {
+  @ApiProperty({
+    example: 'Admin',
+  })
   @IsEnum(Role)
   role: Role;
 }
+
+class EditRoleSuccessResponseDto {
+  @ApiProperty({
+    example: 1,
+  })
+  id: number;
+
+  @ApiProperty({
+    example: 'admin@elab.com',
+  })
+  email: string;
+
+  @ApiProperty({
+    example: 'Admin',
+  })
+  role: Role;
+}
+
+class EditRoleErrorResponseDto {
+  @ApiProperty({
+    example: 'User not found',
+  })
+  message: string;
+
+  @ApiProperty({
+    example: 'Not Found',
+  })
+  error: string;
+
+  @ApiProperty({
+    example: 404,
+  })
+  statusCode: number;
+}
+
+export { EditRoleDto, EditRoleSuccessResponseDto, EditRoleErrorResponseDto };
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, Param, Patch, ValidationPipe } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { EditRoleDto } from './dto/editRole.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  EditRoleDto,
+  EditRoleErrorResponseDto,
+  EditRoleSuccessResponseDto,
+} from './dto/editRole.dto';
 import { ParseIntPipe } from 'src/common/pipes/parseInt.pipe';
 import { UserService } from './user.service';
 import { Role } from '@prisma/client';
@@ -12,6 +16,8 @@ const ROUTE = 'users';
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @ApiResponse({ status: 200, type: EditRoleSuccessResponseDto })
+  @ApiResponse({ status: 401, type: EditRoleErrorResponseDto })
   @Patch(':id/role')
   async editRole(
     @Param('id', ParseIntPipe) userId: number,
