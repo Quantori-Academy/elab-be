d33e4d8776d7750471f5e8782f5c24b6992cd636
armanmikoyan <arman.mikoyan1@gmail.com>
2024-09-30 15:47:45 +0400

    ELAB-BE-20: include auth guard to the api

--- src/common/dtos/parseId.dto.ts
+++ src/common/dtos/parseId.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ParseIdPipeErrorDto {
-  @ApiProperty({ example: 'Invalid value for parameter "id": "-1" is not a valid integer.' })
+  @ApiProperty({ example: 'Invalid value for "id" is not a valid integer.' })
   message: string;
 
   @ApiProperty({ example: 'Bad Request' })
