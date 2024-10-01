88ff0e61b432e4140b8806bba502ab8ea4202943
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-01 14:56:46 +0400

    elab-be-24 update rebase dev

--- src/common/interfaces/repository.interface.ts
+++ src/common/interfaces/repository.interface.ts
@@ -1,4 +1,7 @@
 export interface IRepository<T> {
   findById(id: number): Promise<T | null>;
   update(entity: any): Promise<T>;
+  create(entity: T): Promise<any>;
+  update(entity: T): Promise<any>;
+  save(entity: T): Promise<void>;
 }
