44adc675c756558809535c6b92406252b063b024
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-01 12:57:45 +0400

    elab-be #24: add cors for * origins

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { setupSwagger } from './common/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*', credentials: true });
   app.setGlobalPrefix('api/v1');
   setupSwagger(app);
   await app.listen(3000);
