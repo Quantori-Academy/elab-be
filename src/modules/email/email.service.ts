b44931bd09bb0ed08eed74bdbd2230467f451bd5
Nuray <nuraynurzhaskyzy@gmail.com>
2024-10-02 07:58:23 +0500

    elab-be-30: feature: Add method to send tempPassword to user via email

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -27,7 +27,17 @@ export class EmailService {
       subject: 'Password reset',
       html: `<h4> You requested password reset</h4> <p> Click the link below to reset you password: </p> <p><a href="${url}">Reset Password</a></p>`,
     };
-    this.logger.log('Email is sent to ', email);
+    this.logger.log('Email with reset link is sent to ', email);
+    await this.transporter.sendMail(mailOptions);
+  }
+
+  async sendTempPasswordEmail(email: string, password: string) {
+    const mailOptions = {
+      to: email,
+      subject: 'Password reset',
+      html: `<h4> Your temporary password</h4> <p> Use this temporary password to log in, and create a new password: </p> <p><strong>${password}</strong></p>`,
+    };
+    this.logger.log('Email with temporary password is sent to ', email);
     await this.transporter.sendMail(mailOptions);
   }
 }
