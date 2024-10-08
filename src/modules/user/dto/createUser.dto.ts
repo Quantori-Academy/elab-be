287ac2efa72548b106a7c75a0ea84dcffc997e19
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-08 17:46:24 +0400

    type corrections

--- src/modules/user/dto/createUser.dto.ts
+++ src/modules/user/dto/createUser.dto.ts
@@ -1,23 +1,22 @@
 import { Role } from '@prisma/client';
 import { IUser } from '../interfaces/userEntity.interface';
-import { IsEmail, IsEnum, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsEnum, IsString, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-class CreateUserDto implements IUser {
+class CreateUserDto implements Omit<IUser, 'password'> {
   @ApiProperty({ example: 'user@example.com' })
   @IsEmail({}, { message: 'Invalid email address' })
   email: string;
 
   @ApiProperty({ example: 'Arman' })
+  @IsString()
+  @Matches(/^[A-Z].*/, { message: 'firstName must start with an uppercase letter' })
   firstName: string;
 
   @ApiProperty({ example: 'Mikoyan' })
+  @IsString()
+  @Matches(/^[A-Z].*/, { message: 'lastName must start with an uppercase letter' })
   lastName: string;
-
-  @ApiProperty({ example: 'strongPassword8character' })
-  @IsString({ message: 'Password must be a string' })
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  password: string;
 
   @ApiProperty({ enum: Role })
   @IsEnum(Role)
