eae5b686a0d2b9806da000e3cf97be25a894661f
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-09 22:05:13 +0400

    Seed Storage

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import { UserSeed } from './seeds/seedUser';
 import { Logger } from '@nestjs/common';
+import { StorageSeed } from './seeds/seedStorage';
 
 const prisma = new PrismaClient();
 const logger = new Logger('Seed');
 
 async function main() {
   await UserSeed();
+  await StorageSeed();
   logger.log('seeds are applied');
 }
 
