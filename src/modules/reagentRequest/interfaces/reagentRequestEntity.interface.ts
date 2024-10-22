759a6c35776190d0e2d10f9139aebef570cfd601
Nuray <nuraynurzhaskyzy@gmail.com>
2024-10-22 16:00:12 +0500

    elab-be-90: Add ReagentRequest repository

--- src/modules/reagentRequest/interfaces/reagentRequestEntity.interface.ts
+++ src/modules/reagentRequest/interfaces/reagentRequestEntity.interface.ts
@@ -5,11 +5,11 @@ export interface IReagentRequest {
   name: string;
   userId: number;
   desiredQuantity: string;
-  structureSmiles?: string;
-  structureImage?: string;
-  casNumber?: string;
-  userComments?: string;
-  procurementComments: string;
+  structureSmiles?: string | null;
+  structureImage?: string | null;
+  casNumber?: string | null;
+  userComments?: string | null;
+  procurementComments: string | null;
   status?: Status;
   createdAt?: Date;
   updatedAt?: Date;
