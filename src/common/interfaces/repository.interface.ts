b09c2ffe0e04ca544c99f4fb25143ba557c41779
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-23 21:55:49 +0400

    elab-be-88 #88: change IRepository update method param type

--- src/common/interfaces/repository.interface.ts
+++ src/common/interfaces/repository.interface.ts
@@ -1,8 +1,10 @@
+import { PartialWithRequiredId } from '../types/idRequired.type';
+
 export interface IRepository<T, R = any> {
   findById(id: number): Promise<T | null>;
   findAll(): Promise<R>;
   create(entity: T): Promise<T>;
-  update(entity: Partial<T>): Promise<T>;
+  update(entity: PartialWithRequiredId<T>): Promise<T>;
   upsert(entity: T): Promise<void>;
   delete(id: number): Promise<T>;
 }
