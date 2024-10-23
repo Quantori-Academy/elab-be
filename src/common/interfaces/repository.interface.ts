2211ed29f6e97b664dee57deb17f634407a1a209
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-23 22:05:28 +0400

    update IRepository type

--- src/common/interfaces/repository.interface.ts
+++ src/common/interfaces/repository.interface.ts
@@ -1,10 +1,8 @@
-import { PartialWithRequiredId } from '../types/idRequired.type';
-
 export interface IRepository<T, R = any> {
   findById(id: number): Promise<T | null>;
   findAll(): Promise<R>;
   create(entity: T): Promise<T>;
-  update(entity: PartialWithRequiredId<T>): Promise<T>;
+  update(entity: T): Promise<T>;
   upsert(entity: T): Promise<void>;
   delete(id: number): Promise<T>;
 }
