e72f53a9b159ecee9074e3f29370cf14b2e2efe9
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-30 15:08:51 +0400

    elab-be-123 #123 [change] variable name

--- src/modules/room/types/room.type.ts
+++ src/modules/room/types/room.type.ts
@@ -1,17 +1,21 @@
-import { Room, Storage } from '@prisma/client';
+import { Prisma, Room, Storage } from '@prisma/client';
 
 type RoomWithStorages = Room & {
   storages: Storage[];
 };
 
+type RoomWithStorageCountObject = Prisma.RoomGetPayload<{
+  include: {
+    _count: {
+      select: {
+        storages: true;
+      };
+    };
+  };
+}>;
+
 type RoomWithStorageCount = Room & {
   storageCount: number;
-};
-
-type RoomWithStorageCountObject = Room & {
-  _count: {
-    storages: number;
-  };
 };
 
 type IdOnly = {
