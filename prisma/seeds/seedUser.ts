134843ba93525af9e827517199aa0b1b594afcb6
armanmikoyan <arman.mikoyan1@gmail.com>
2024-10-31 20:06:50 +0400

    elab-be-138 #138 [update] seeding

--- prisma/seeds/seedUser.ts
+++ prisma/seeds/seedUser.ts
@@ -1,38 +1,46 @@
+import { Logger } from '@nestjs/common';
 import { PrismaClient, Role } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { IUser } from 'src/modules/user/interfaces/userEntity.interface';
 
 const prisma = new PrismaClient();
+const logger = new Logger(UserSeed.name);
 
 export async function UserSeed() {
   const users: IUser[] = [
-    { email: 'admin@elab.com', firstName: "firstName", lastName: "firstName", password: 'Admin_123', role: Role.Admin },
-    { email: 'nurai@elab.com',  firstName: "firstName", lastName: "firstName", password: 'Nurai_456', role: Role.Admin },
+    { email: 'admin@elab.com', firstName: "Arman", lastName: "Mikoyan", password: 'Admin_123', role: Role.Admin },
+    { email: 'nurai@elab.com',  firstName: "Noah", lastName: "Jones", password: 'Nurai_456', role: Role.Admin },
     {
       email: 'arman@elab.com',
-      firstName: "firstName",
-      lastName: "firstName",
+      firstName: "Ava",
+      lastName: "Davis",
       password: 'Arman_789',
       role: Role.ProcurementOfficer,
     },
     {
       email: 'bob@elab.com',
-      firstName: "firstName",
-      lastName: "firstName",
+      firstName: "Isabella",
+      lastName: "Wilson",
       password: 'Bob_123',
       role: Role.ProcurementOfficer,
     },
-    { email: 'jane@elab.com', firstName: "firstName", lastName: "firstName", password: 'Jane_432', role: Role.Researcher },
-    { email: 'lena@elab.com', firstName: "firstName", lastName: "firstName", password: 'Lena_876', role: Role.Researcher },
-    { email: 'mark@elab.com', firstName: "firstName", lastName: "firstName", password: 'Mark_739', role: Role.Researcher },
-    { email: 'jack@elab.com', firstName: "firstName", lastName: "firstName", password: 'Jack_720', role: Role.Researcher },
-    { email: 'karya@elab.com',firstName: "firstName", lastName: "firstName", password: 'Karya_007', role: Role.Researcher },
+    { email: 'jane@elab.com', firstName: "Ethan", lastName: "Brown", password: 'Jane_432', role: Role.Researcher },
+    { email: 'lena@elab.com', firstName: "Mia", lastName: "Jones", password: 'Lena_876', role: Role.Researcher },
+    { email: 'mark@elab.com', firstName: "Oliver", lastName: "Thomas", password: 'Mark_739', role: Role.Researcher },
+    { email: 'jack@elab.com', firstName: "Ethan", lastName: "Jones", password: 'Jack_720', role: Role.Researcher },
+    { email: 'karya@elab.com',firstName: "Isabella", lastName: "firstName", password: 'Karya_007', role: Role.Researcher },
   ];
 
   for (const user of users) {
     await prisma.user.upsert({
       where: { email: user.email },
-      update: {},
+      update: {
+       email: user.email,
+       firstName: user.firstName,
+       lastName: user.lastName,
+       password: await bcrypt.hash(user.password, 10),
+       role: user.role,
+      },
       create: {
         email: user.email,
         firstName: user.firstName,
@@ -42,4 +50,5 @@ export async function UserSeed() {
       },
     });
   }
+  logger.log('User seed completed');
 }
