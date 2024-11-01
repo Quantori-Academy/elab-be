1a657ffe8d2675bde8680cf03b03cf94378f6ba1
armanmikoyan <101734083+armanmikoyan@users.noreply.github.com>
2024-11-01 17:19:34 +0400

    [BE] List Order API (#136)
    * elab-be-111 #111 [add] layers methods
    
    * elab-be-111 #111 [add] swagger documentation
    
    * elab-be-111 #111 [update] access role
    
    * elab-be-111 #111 [update] access role
    
    * elab-be-111 #111 [add] filter by status
    
    * elab-be-111 #111 [add] sort by seller/title
    
    * elab-be-111 #111 [update] typographical mistake

--- src/modules/order/interfaces/orderService.interface.ts
+++ src/modules/order/interfaces/orderService.interface.ts
@@ -1,5 +1,7 @@
-import { CompleteOrderData, OrderWithReagents } from '../types/order.type';
+import { CompleteOrderData, OrderList, OrderWithReagents } from '../types/order.type';
+import { OrdereOptions } from '../types/orderOptions.type';
 
 export interface IOrderService {
   createOrder(orderDto: CompleteOrderData): Promise<OrderWithReagents>;
+  orderList(options: OrdereOptions): Promise<OrderList>;
 }
--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, HttpStatus, Inject, Logger, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Inject, Logger, Post, Query, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 import { ORDER_SERVICE_TOKEN } from './order.service';
 import { IOrderService } from './interfaces/orderService.interface';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CompleteOrderData, OrderWithReagents } from './types/order.type';
+import { ApiBearerAuth, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CompleteOrderData, OrderList, OrderWithReagents } from './types/order.type';
 import { CreateOrderBadRequestDto, CreateOrderDto, CreateOrderNotFoundDto, CreateOrderSuccessDto } from './dto/createOrder.dto';
 import { Request } from 'express';
 import { UserPayload } from '../user/interfaces/userEntity.interface';
@@ -12,6 +12,9 @@ import { AuthGuard } from '../auth/guards/auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { TokenErrorResponseDto } from '../security/dto/token.dto';
 import { ForbiddenErrorDto } from 'src/common/dtos/forbidden.dto';
+import { ValidateParseOrderOptionsPipe } from './pipes/validateParseQueries..pipe';
+import { OrdereOptions } from './types/orderOptions.type';
+import { GetOrderListResponseDto, GetOrdersQueryDto, GetOrderValidationErrorsDto } from './dto/getOrder.dto';
 
 const ROUTE = 'orders';
 
@@ -47,6 +50,27 @@ export class OrderController {
       return order;
     } catch (error) {
       this.logger.error(`[${this.createOrder.name}] - Exception thrown: ` + error);
+      throw error;
+    }
+  }
+
+  @ApiBearerAuth()
+  @ApiQuery({ type: GetOrdersQueryDto })
+  @ApiResponse({ status: HttpStatus.OK, type: GetOrderListResponseDto })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, type: GetOrderValidationErrorsDto })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, type: ForbiddenErrorDto })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, type: TokenErrorResponseDto })
+  @Roles(Role.ProcurementOfficer)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Get('')
+  async orderList(@Query(ValidateParseOrderOptionsPipe) options: OrdereOptions): Promise<OrderList> {
+    this.logger.log(`[${this.orderList.name}] - Method start`);
+    try {
+      const order: OrderList = await this.orderService.orderList(options);
+      this.logger.log(`[${this.orderList.name}] - Method finished`);
+      return order;
+    } catch (error) {
+      this.logger.error(`[${this.orderList.name}] - Exception thrown: ` + error);
       throw error;
     }
   }
