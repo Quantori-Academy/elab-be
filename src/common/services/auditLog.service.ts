92da3c554b3bacbd31b118b81cb5e2c93f42767e
Nuray <nuraynurzhaskyzy@gmail.com>
2024-12-05 10:24:56 +0500

    elab-be-114: Add getHistory method

--- src/common/services/auditLog.service.ts
+++ src/common/services/auditLog.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { PrismaService } from "src/modules/prisma/prisma.service";
 import { AuditBody } from "../types/auditBody.type";
+import { AuditLogs, Entity } from "@prisma/client";
 
 @Injectable()
 export class AuditLogService {
     constructor(private prisma: PrismaService) {}
     private logger = new Logger(AuditLogService.name);
 
-    async createAuditLog(auditBody: AuditBody) {
+    async createAuditLog(auditBody: AuditBody): Promise<void> {
         try {
             this.logger.log(`${this.createAuditLog.name} - START`);
             await this.prisma.auditLogs.create({
@@ -15,8 +16,25 @@ export class AuditLogService {
             })
             this.logger.log(`${this.createAuditLog.name} - FINISH`);
         } catch (error) {
-            this.logger.error(`${this.createAuditLog.name} - ERROR`, error.message);
+            this.logger.error(`${this.createAuditLog.name} - ERROR`, error);
             throw error;
+        }
+    }
+
+    async getHistory(entity: Entity): Promise<AuditLogs[] | null> {
+        try {
+            this.logger.log(`${this.getHistory.name} - START`);
+            const history = await this.prisma.auditLogs.findMany({
+                where: {entity},
+                orderBy: {
+                    timestamp: 'desc'
+                }
+            });
+            this.logger.log(`${this.getHistory.name} - FINISH`); 
+            return history;
+        } catch (error) {
+            this.logger.error(`${this.getHistory.name} - ERROR`, error);
+            throw error; 
         }
     }
 }
